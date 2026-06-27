dcc0059e8b82476594ce699bf6515fdf55211232
Carlos Costa <ccostaeconomia@gmail.com>
2026-06-27 18:58:21 -0300

    Hit box colision HUB Score

--- code/Const.py
+++ code/Const.py
@@ -1,9 +1,11 @@
 import pygame
 
 # C
-COLOR_ORANGE = (255, 120, 0)
+C_ORANGE = (255, 120, 0)
 C_WHITE = (255, 255, 255)
 C_YELLOW = (255, 255, 0)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
@@ -98,7 +100,7 @@ ENTITY_SCORE = {
     'Enemy1Shot': 0,
     'Enemy2': 125,
     'Enemy2Shot': 0,
-}       
+}
 
 ENTITY_SHOT_DELAY = {
     'Player1': 20,
@@ -129,7 +131,27 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
 # S
 SPAWN_TIME = 4000
 
+# S
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
+
+# T
+TIMEOUT_STEP = 100  # 100ms
+TIMEOUT_LEVEL = 20000  # 20s
 
 # W
-WINDOW_WIDTH = 576
-WINDOW_HEIGHT = 324
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -3,7 +3,7 @@
 import random
 
 from code.Background import Background
-from code.Const import WINDOW_WIDTH, WINDOW_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.Player import Player
 
 
@@ -16,14 +16,14 @@ class EntityFactory:
                 list_bg = []
                 for i in range(7):  # level images number
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
-                    list_bg.append(Background(f'Level1Bg{i}', (WINDOW_WIDTH, 0)))
+                    list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg #
 
             case 'Player1':
-                return Player(f'Player1', (10, WINDOW_HEIGHT / 2 - 30))
+                return Player(f'Player1', (10, WIN_HEIGHT / 2 - 30))
             case 'Player2':
-                return Player(f'Player2', (10, WINDOW_HEIGHT / 2 + 30))
+                return Player(f'Player2', (10, WIN_HEIGHT / 2 + 30))
             case 'Enemy1':
-                return Player(f'Enemy1', (WINDOW_WIDTH + 10, random.randint(40, WINDOW_HEIGHT - 40)))
+                return Player(f'Enemy1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
             case 'Enemy2':
-                return Player(f'Enemy2', (WINDOW_WIDTH + 10, random.randint(40, WINDOW_HEIGHT - 40)))
+                return Player(f'Enemy2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
